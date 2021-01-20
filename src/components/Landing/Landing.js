78690345c2f121df3bfd0e7fd0dfe7564c274b2a
Gabriel Muller <gabriel.muller.12@gmail.com>
2021-01-20 22:07:10 +0100

    finish up index

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,9 +6,9 @@ import React from 'react';
 const Landing = ({ cover, title, description }) => {
   return (
     <div className="landing">
-      <Img fluid={cover} className="img" alt={title}/>
-      <h1 className="title">{title}</h1>
-      <h2 className="description">{description}</h2>
+      <Img fluid={cover} className="img" alt={title} itemProp="image"/>
+      <h1 className="title" itemProp="name headline about">{title}</h1>
+      <h2 className="description" itemProp="description abstract">{description}</h2>
     </div>
   )
 }
--- src/components/PostSidebar/PostSidebar.js
+++ src/components/PostSidebar/PostSidebar.js
@@ -13,19 +13,22 @@ const PostSidebar = ({ posts, className, title }) => {
       <div className="post-sidebar-title">
         <Link to="/">{title}</Link>
       </div>
-      <ol className="post-sidebar-list">
-        {posts.map(post => {
+      <ol className="post-sidebar-list" itemScope itemType="https://schema.org/ItemList">
+        {posts.map((post, index) => {
           const title = post.frontmatter.title || post.fields.slug
           const img = post.frontmatter.cover.childImageSharp.fixed
 
           return (
             <li key={post.fields.slug}>
               <article
-                itemScope itemType="https://schema.org/BlogPosting"
+                itemProp="itemListElement"
+                itemScope
+                itemType="https://schema.org/ListItem"
               >
                 <Link to={post.fields.slug} itemProp="url">
+                  <meta itemprop="position" content={index + 1} />
                   <div className="item-img-wrapper"><Img fixed={img} itemProp="image" className="item-img" alt={title}/></div>
-                  <span itemProp="headline name" className="item-text">{title}</span>
+                  <span itemProp="name" className="item-text">{title}</span>
                 </Link>
               </article>
             </li>
